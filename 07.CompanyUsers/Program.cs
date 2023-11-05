a9e527ac892011dde44974f78c629118d40e1864
Ivan Kratchanov <ivankra@gmail.com>
2023-11-05 17:46:45 +0200

    Add files via upload

---
+++ 07.CompanyUsers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+        string input = Console.ReadLine();
+
+        while (input != "End")
+        {
+            string[] inputArgs = input.Split(" -> ");
+            string companyName = inputArgs[0];
+            string employeeId = inputArgs[1];
+
+            if (!companies.ContainsKey(companyName))
+            {
+                companies[companyName] = new List<string>();
+            }
+
+            if (!companies[companyName].Contains(employeeId))
+            {
+                companies[companyName].Add(employeeId);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        foreach (var company in companies)
+        {
+            Console.WriteLine(company.Key);
+            foreach (var employeeId in company.Value)
+            {
+                Console.WriteLine($"-- {employeeId}");
+            }
+        }
+    }
+}
